0d557b9e7e2d0a1198321483b1fbe1850102f032
AZHARULLAH <shariffazharullah@gmail.com>
2016-09-25 15:07:14 +0530

    Few colour and style changes pending

---
+++ js/home.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+})
+
+// $(".fadeaway").css("opacity", 0.4);
+// $("#myimagemain").css("opacity", 1);
+
+// $(window).scroll(function()
+// {
+// 	var winheight = $(window).scrollTop();
+//     	// $(".fadeaway").css("opacity", 0.4 + winheight / 450);
+//     	var dim = winheight / 15;
+//     	if(dim < 65)
+//     	{
+// 		$("#res_img").height(dim);
+// 		$("#res_img").width(dim);
+//     	}
+//     	var imdim = 200 - winheight / 3;
+//     	if(imdim > 60)
+//     	{
+//     		$("#myimagemain").height(imdim);
+//     		$("#myimagemain").width(imdim);
+//     	}
+
+// });
+
