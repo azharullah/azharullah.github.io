2b3b1faa60b0b75dc3c9db00156c2f62a3858288
MD. AZHARULLAH SHARIFF <shariffazharullah@gmail.com>
2017-03-30 01:33:31 +0530

    Added presentations, updated projects and a few design changes!

--- js/home.js
+++ js/home.js
@@ -1,40 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
 
-	$('.scrollspy').scrollSpy();
+    $('.scrollspy').scrollSpy();
 
-	var actdim = $("#myimagemain").height()
+    var actdim = $("#myimagemain").height()
 
 
-	$(window).scroll(function()
-	{
-		var winheight = $(window).scrollTop();
-	    	// $(".fadeaway").css("opacity", 0.4 + winheight / 450);
-	    	var dim = winheight / 10;
-	    	if(dim < 65)
-	    	{
-			$("#res_img").height(dim);
-			$("#res_img").width(dim);
-	    	}
-	    	var imdim = actdim - winheight / 3;
-	    	if(imdim > 60)
-	    	{
-	    		$("#myimagemain").height(imdim);
-	    		$("#myimagemain").width(imdim);
-	    	}
+    $(window).scroll(function() {
+        var winheight = $(window).scrollTop();
+        // $(".fadeaway").css("opacity", 0.4 + winheight / 450);
+        var dim = winheight / 8;
+        if (dim < 65) {
+            $("#res_img").height(dim);
+            $("#res_img").width(dim);
+        }
+        var imdim = actdim - winheight / 3;
+        if (imdim > 80) {
+            $("#myimagemain").height(imdim);
+            $("#myimagemain").width(imdim);
+        }
 
-	});
+    });
 
-	$(".brand-logo").click(function(){
-		jQuery('html,body').animate({scrollTop:0},3000);
-	})
+    $(".brand-logo").click(function() {
+        jQuery('html,body').animate({ scrollTop: 0 }, 3000);
+    })
 
-	// $(document).bind("contextmenu copy paste cut drag drop ",function(e) {return false;});
+    // $(document).bind("contextmenu copy paste cut drag drop ",function(e) {return false;});
 
 
 })
 
 // $(".fadeaway").css("opacity", 0.4);
 // $("#myimagemain").css("opacity", 1);
-
