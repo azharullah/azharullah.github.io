a992bf9640a9523fa4045791bffd4fc532e04a29
MD. AZHARULLAH SHARIFF <shariffazharullah@gmail.com>
2017-03-30 01:41:52 +0530

    A few design fixes and added presentations and resume locally

--- js/home.js
+++ js/home.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
-    $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+        // menuWidth: 300, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
 
     $('.scrollspy').scrollSpy();
 
